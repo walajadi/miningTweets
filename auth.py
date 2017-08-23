0a3dddfc5e6ba27be75ce3668d88973873eaf412
slim <=>
2017-08-23 22:22:09 +0200

    build oauth obj with requests_oauth

--- auth.py
+++ auth.py
@@ -1,3 +1,12 @@
-client_key = 'xxx'
-client_secret = 'xxx'
+import requests_oauthlib
 
+access_token = 'xxx'
+access_secret = 'xxx'
+consumer_key = 'xxx'
+consumer_secret = 'xxx'
+
+my_auth = requests_oauthlib.OAuth1(consumer_key,
+	consumer_secret,
+	access_token,
+	access_secret)
+ 
